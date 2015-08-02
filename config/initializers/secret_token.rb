0f6926b8f87b685f4ad292b22624c8477582d0a7
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-08-02 14:02:25 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feellikeplaying::Application.config.secret_key_base = '7cef991fd0c6ef89bf303f34735842670b7b17fdad238cf055d4cc43f7e8047c470500118b7eb156ff79c2701abc923e5609fd1169310588a26d60c93b8222a0'
